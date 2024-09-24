56e66179fb96020979bd083428e6e6c67268beda
Sandhya K <115928773+Sandhyakaruna@users.noreply.github.com>
2024-09-24 23:05:26 +0530

    Update doctor's survey results.py

--- doctor's survey results.py
+++ doctor's survey results.py
@@ -1,12 +1,12 @@
 def find_non_working_doctors():
     try:
-        # Input size of the first list
+       
         n1 = int(input())
         if n1 <= 0:
             print("Invalid list size")
             return
         
-        # Input elements for the first list
+       
         first_list = []
         for _ in range(n1):
             doctor_id = int(input())
@@ -15,7 +15,7 @@ def find_non_working_doctors():
                 return
             first_list.append(doctor_id)
         
-        # Input size of the second list
+       
         n2 = int(input())
         if n2 <= 0:
             print("Invalid list size")
